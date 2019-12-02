bfb164d6c0ba96ccea162257a02bfce5294c4c3a
aemarco <4aemarco@gmail.com>
2019-12-02 20:08:17 +0100

    Projektdateien hinzufügen.

---
+++ ClassTemplates/GlobalSuppressions.cs
@@ -0,0 +1,6 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Codequalität", "IDE0052:Ungelesene private Member entfernen", Justification = "<Ausstehend>", Scope = "member", Target = "~F:ClassTemplates.SimpleClass._log")]
---
+++ ClassTemplates/SimpleClass.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassTemplates
+{
+    /// <summary>
+    /// Purpose:
+    /// Created by: $username$
+    /// Created at: $time$
+    /// </summary>
+    public class SimpleClass
+    {
+        #region ctor
+
+        private readonly ILogger<SimpleClass> _log;
+        public SimpleClass(ILogger<SimpleClass> log)
+        {
+            _log = log;
+        }
+
+        #endregion
+
+    }
+}
